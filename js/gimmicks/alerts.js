89ee6941e3a316ed96a6bd4c6183ca5933a3fd19
Timo Dörr <timo@latecrew.de>
2013-05-13 13:28:55 +0200

    Extend alerts to support notes,hint and warnings

--- js/gimmicks/alerts.js
+++ js/gimmicks/alerts.js
@@ -1,4 +1,5 @@
 (function($) {
+    'use strict';
     var methods = {
         // takes a standard <img> tag and adds a hyperlink to the image source
         // needed since we scale down images via css and want them to be accessible
@@ -6,34 +7,59 @@
         alerts: function() {
 
             var select_paragraphs = function(expressions) {
-                var $exp = $([ "achtung", "attention", "warnung", "warning", "atención", "guarda", "advertimiento" ]);
-                var matched_p = [];
-                $("p").filter (function () {
+                var note = ['note', 'beachte' ];
+                var warning = [ 'achtung', 'attention', 'warnung', 'warning', 'atención', 'guarda', 'advertimiento' ];
+                var hint = ['hint', 'tipp', 'hinweis'];
+                var exp = note.concat(warning);
+                exp = exp.concat(hint);
+                var matches = [];
+
+                $('p').filter (function () {
                     var $par = $(this);
                     // check against each expression
-                    $exp.each (function (i,trigger) {
+                    $(exp).each (function (i,trigger) {
                         var txt = $par.text().toLowerCase ();
                         // we match only paragrachps in which the 'trigger' expression
                         // is follow by a ! or :
                         var re = new RegExp (trigger + '(:|!)+.*','i');
+                        var alertType = 'none';
                         if (txt.match (re) !== null) {
-                            matched_p.push ($par);
+                            if ($.inArray(trigger, note) >= 0) {
+                                alertType = 'note';
+                            } else if ($.inArray(trigger, warning) >= 0) {
+                                alertType = 'warning';
+                            } else if ($.inArray(trigger, hint) >= 0) {
+                                alertType = 'hint';
+                            }
+                            matches.push ({
+                                p: $par,
+                                alertType: alertType
+                            });
                         }
                     });
                 });
-                return matched_p;
+                return matches;
             };
 
-            var $par;
+            var matches;
             if (!(this instanceof jQuery)) {
                 // return those paragraphes
-                $par = $(select_paragraphs ());
+                matches = $(select_paragraphs ());
             } else {
-                $par = $(this);
+                matches = { alertType: 'note', p: this };
             }
-            return $par.each(function() {
-                var $this = $(this);
-                $this.addClass ("alert");
+            return matches.each(function() {
+                var $p = $(this.p);
+                var type = this.alertType;
+                $p.addClass('alert');
+
+                if (type === 'note') {
+                    //$p.addClass('');
+                } else if (type === 'hint') {
+                    $p.addClass('alert-success');
+                } else if (type === 'warning') {
+                    $p.addClass('alert-error');
+                }
             });
         }
     };
