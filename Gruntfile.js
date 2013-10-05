83b0c0aa829408cfefa11c6d64f09ed335b5c2e1
Timo Dörr <timo@latecrew.de>
2013-10-05 15:39:17 +0200

    Bump version to 0.5.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
     // Metadata.
         pkg: {
             name: 'MDwiki',
-            version: '0.5.0'
+            version: '0.5.1'
         },
 
         ownJsFiles: [
