c90d5a22f9b7bc311ca219de8f5656bebef1f7c1
Curtis Mitchell <curtismitchell@gmail.com>
2013-12-06 11:10:08 -0500

    Fix jump links
    * only add jump links if the side menu is used
    * only add jump to H2s
    
    Signed-off-by: Timo Dörr <timo@latecrew.de>

--- js/basic_skeleton.js
+++ js/basic_skeleton.js
@@ -193,6 +193,9 @@
 
         // adds a link to the navigation at the top of the page
         function addJumpLinkToTOC($heading) {
+            if($.md.config.useSideMenu === false) return;
+            if($heading.prop("tagName") !== 'H2') return;
+
             var c = $.md.config.tocAnchor;
             if (c === '')
                 return;
