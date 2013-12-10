91cac2ed1fcbf35af2ea62a431f6389efecb2abc
Dean Sofer <ProLoser@users.noreply.github.com>
2013-12-10 14:46:29 -0800

    Changed path.js to editme.js
    I don't like having this link-automagic crap because then I have less power to customize things, but since it appears you can't nest gimmicks, I converted this to your standard linkGimmick.
    
    `[gimmick:editMe(text:'Edit this File')](https://github.com/ProLoser/webshow/edit/gh-pages/)`
    
    The `text` option is optional, the url is the prefix of the file path.

---
+++ js/gimmicks/editme.js
@@ -0,0 +1,18 @@
+(function($){
+    'use strict';
+    var editMeGimmick = {
+        name: 'editme',
+        version: $.md.version,
+        once: function() {
+            $.md.linkGimmick(this, 'editme', editMe);
+        }
+    };
+    $.md.registerGimmick(editMeGimmick);
+ 
+    function editMe($links, opt, href) {
+        opt.text = opt.text || 'Edit Me'
+        return $links.each(function(i,link) {
+            $(link).text(opt.text).attr('href', href + $.md.mainHref);
+        });
+    }
+}(jQuery));
