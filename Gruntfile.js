b7dc3967453a12192ef43deeb7ce3df240ecf952
Isidro Rodríguez González <isirguezglez@gmail.com>
2014-04-17 19:04:48 +0100

    Compile all CSS through a main LESS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -60,9 +60,7 @@ module.exports = function(grunt) {
         // * ORDER OF FILES IS IMPORTANT
         // * ALWAYS ADD EACH FILE TO BOTH minified/unminified SECTIONS!
         cssFiles: [
-            'tmp/bootstrap.min.css',
-            'extlib/css/prism.default.css',
-            'extlib/css/colorbox.css',
+            'tmp/main.min.css',
         ],
         jsFiles: [
             'bower_components/jquery/jquery.min.js',
@@ -73,9 +71,7 @@ module.exports = function(grunt) {
         ],
         // for debug builds use unminified versions:
         unminifiedCssFiles: [
-            'tmp/bootstrap.css',
-            'extlib/css/colorbox.css',
-            'extlib/css/prism.default.css',
+            'tmp/main.css'
         ],
         unminifiedJsFiles: [
             'bower_components/jquery/jquery.js',
@@ -106,7 +102,7 @@ module.exports = function(grunt) {
                     compress: true,
                 },
                 files: {
-                    'tmp/bootstrap.min.css': 'extlib/less/bootstrap.less',
+                    'tmp/main.min.css': 'styles/main.less',
                 },
             },
             dev: {
@@ -114,7 +110,7 @@ module.exports = function(grunt) {
                     compress: false,
                 },
                 files: {
-                    'tmp/bootstrap.css': 'extlib/less/bootstrap.less',
+                    'tmp/main.css': 'styles/main.less',
                 },
             },
         },
