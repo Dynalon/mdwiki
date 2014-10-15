3cc5fea5ea2d6b97a1204a39e0d663a5826f856b
Timo Dörr <timo@latecrew.de>
2014-10-15 19:03:15 +0200

    Adapt alerts as a link gimmick with new api

--- js/gimmicks/alerts.js
+++ js/gimmicks/alerts.js
@@ -1,67 +1,71 @@
 (function($) {
-    //'use strict';
-    var alertsModule = new MDwiki.Core.Module();
-    alertsModule.init = function() {
-        $.md.stage('bootstrap').subscribe(function(done) {
-            createAlerts();
-            done();
-        });
+
+    // new gimmick API
+    var handler = new MDwiki.Gimmick.GimmickHandler('singleline');
+    handler.callback = function(a, b, domElement) {
+        domElement.replaceWith("<h1>SINGLELINE!</h1>");
     };
-    $.md.wiki.gimmicks.registerModule(alertsModule);
+    var handler2 = new MDwiki.Gimmick.GimmickHandler('multiline');
+    handler2.callback = function(a, b, domElement) {
+        domElement.replaceWith("<h1>MULTILINE!</h1>");
+    };
+    var handler3 = new MDwiki.Gimmick.GimmickHandler('link');
+    handler3.callback = function(a, b, c, domElement) {
+        domElement.replaceWith("<h1>LINK!</h1>");
+    };
 
-    // takes a standard <img> tag and adds a hyperlink to the image source
-    // needed since we scale down images via css and want them to be accessible
-    // in original format
-    function createAlerts() {
-        var matches = $(select_paragraphs());
-        matches.each(function() {
-            var $p = $(this.p);
-            var type = this.alertType;
-            $p.addClass('alert');
+    var gmck = new MDwiki.Gimmick.Gimmick('foobar', handler);
+    gmck.addHandler(handler2);
+    gmck.addHandler(handler3);
 
-            if (type === 'note') {
-                $p.addClass('alert-info');
-            } else if (type === 'hint') {
-                $p.addClass('alert-success');
-            } else if (type === 'warning') {
-                $p.addClass('alert-warning');
-            }
-        });
-    }
+    $.md.wiki.gimmicks.registerGimmick(gmck);
 
-    // picks out the paragraphs that start with a trigger word
-    function select_paragraphs() {
+    //'use strict';
+    var alertsGimmick = new MDwiki.Gimmick.Gimmick('alert');
+    var alertHandler = new MDwiki.Gimmick.GimmickHandler('link');
+    alertHandler.callback = function(trigger, text, options, domElement) {
+        var type = get_alert_type(text);
+        if (type === null) return;
+
+        var $p = $('<p/>');
+        $p.addClass('alert');
+        if (type === 'note') {
+            $p.addClass('alert-info');
+        } else if (type === 'hint') {
+            $p.addClass('alert-success');
+        } else if (type === 'warning') {
+            $p.addClass('alert-warning');
+        }
+        $p.text(text);
+        $(domElement).replaceWith($p);
+    };
+    alertsGimmick.addHandler(alertHandler);
+    $.md.wiki.gimmicks.registerGimmick(alertsGimmick);
+
+    function get_alert_type(text) {
         var note = ['note', 'beachte' ];
         var warning = [ 'achtung', 'attention', 'warnung', 'warning', 'atención', 'guarda', 'advertimiento' ];
         var hint = ['hint', 'tipp', 'tip', 'hinweis'];
         var exp = note.concat(warning);
         exp = exp.concat(hint);
-        var matches = [];
+        var txt = text.toLowerCase ();
 
-        $('p').filter (function () {
-            var $par = $(this);
-            // check against each expression
-            $(exp).each (function (i,trigger) {
-                var txt = $par.text().toLowerCase ();
-                // we match only paragrachps in which the 'trigger' expression
-                // is follow by a ! or :
-                var re = new RegExp (trigger + '(:|!)+.*','i');
-                var alertType = 'none';
-                if (txt.match (re) !== null) {
-                    if ($.inArray(trigger, note) >= 0) {
-                        alertType = 'note';
-                    } else if ($.inArray(trigger, warning) >= 0) {
-                        alertType = 'warning';
-                    } else if ($.inArray(trigger, hint) >= 0) {
-                        alertType = 'hint';
-                    }
-                    matches.push ({
-                        p: $par,
-                        alertType: alertType
-                    });
+        // check against each expression
+        var returnval = null;
+        $(exp).each (function (i,trigger) {
+            // we match only paragrachps in which the 'trigger' expression
+            // is follow by a ! or :
+            var re = new RegExp (trigger + '(:|!)+.*','i');
+            if (txt.match (re) !== null) {
+                if ($.inArray(trigger, note) >= 0) {
+                    returnval = 'note';
+                } else if ($.inArray(trigger, warning) >= 0) {
+                    returnval = 'warning';
+                } else if ($.inArray(trigger, hint) >= 0) {
+                    returnval = 'hint';
                 }
-            });
+            }
         });
-        return matches;
+        return returnval;
     }
 }(jQuery));
