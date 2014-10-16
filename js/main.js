d2148703240cf00d4e21dbd7d4358e32135b922b
Timo Dörr <timo@latecrew.de>
2014-10-16 11:13:59 +0200

    Add check for Specrunner/unittest env

--- js/main.js
+++ js/main.js
@@ -65,7 +65,10 @@
         });
     };
 
-    $.initMDwiki(undefined, true);
+    if (window.location.href.indexOf('SpecRunner') >= 0)
+        $.initMDwiki(undefined, false);
+    else
+        $.initMDwiki(undefined, true);
 
 
 }(jQuery));
