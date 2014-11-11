b391c2eaf8b0b3cc08a37414eae22ae4b0026c22
Timo Dörr <timo@latecrew.de>
2014-11-11 21:27:42 +0100

    Add test form images in paragraph

--- unittests/spec/test_basicskeleton.js
+++ unittests/spec/test_basicskeleton.js
@@ -13,8 +13,19 @@ describe('BasicSkeleton', function() {
         var section = $('#single-paragraph')[0];
         skeleton = new MDwiki.Legacy.PageSkeleton(config, section);
         skeleton.createBasicSkeleton();
-        debugger;
         var text = $.trim($(section).find('.md-text .md-paragraph-content').text());
         expect(text).toBe('This is a sentence.');
+    });
+
+    it('should have moved left floated images out of the paragraph', function() {
+        var section = $('#single-paragraph-with-image')[0];
+        skeleton = new MDwiki.Legacy.PageSkeleton(config, section);
+        skeleton.createBasicSkeleton();
+
+        var number_of_images_in_paragraph = $(section).find('p img').length;
+        expect(number_of_images_in_paragraph).toBe(0);
+        var image_in_floatenv = $(section).find('.md-text .md-float-left img').length === 1;
+        debugger;
+        expect(image_in_floatenv).toBeTruthy();
     });
 });
