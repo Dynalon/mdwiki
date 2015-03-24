22d0426733694460c326508f166ab63e12412691
Jeremy Corson <jcorson@dekaresearch.com>
2015-03-24 10:11:43 -0400

    At this point i have multiple charts rendering from a single table of data.Charts are responsive, animated, but offer no styling options.

--- js/gimmicks/chart.js
+++ js/gimmicks/chart.js
@@ -21,26 +21,40 @@
 
     function chart($links, opt, text) {
         return $links.each (function (i, link){
-            var $link = $(link);
-            var myHtml = $('<canvas id="chart"></canvas>');
-            $link.replaceWith(myHtml);
 
-            // default options
+            // Get the options for this gimmick
+            //      labelColumn: This is a string the indicates the column that will be used to
+            //                   label the data points
+            //      dataColumns: This is a array of strings that indicate each column to be plotted
+            //      canvasId:    This is a ID for the given chart. Defaults to a random number
+            //                   between 1-1000.
             var default_options = {
-                chartType: 'line',
-                labelColumn: 'Sprint',
-                seriesColumn: ['Avg', 'Sum']
+                canvasId: Math.floor((Math.random() * 1000) + 1)
             };
             var options = $.extend ({}, default_options, opt);
 
-            // This is the object that is given to the chart frame work for rendering
+            //chartoptions
+            var chartOptions = {
+                responsive: true
+            };
+
+            // Replace the Gimmick with the canvas that chartJS needs
+            var $link = $(link);
+            var myHtml = $('<canvas id="' + options.canvasId + '" ></canvas>');
+            $link.replaceWith(myHtml);
+
+            // This is the object that is given to the chart frame work for rendering. It will be
+            // built up by processing the html table that is found on the table.
             var chartConfig = {
                 "datasets": [],
                 "labels": []
             };
 
+            // These variables hold the indices of the columns in the table we care about. They will
+            // be populated as we process the table based on the options that are given in the
+            // gimmick
             var labelColumnIndex = -1;
-            var dataColumnIndex = [];
+            var dataColumnIndices = [];
 
             // Get the index of the columns that we care about for charting
             // TODO JC Currently assumes that there is a single table on the page. Document this or
@@ -54,9 +68,9 @@
 
                 // Check if this is a data column
                 else {
-                    for(var i = 0; i < options.seriesColumn.length; i++) {
-                        if(this.textContent === options.seriesColumn[i]) {
-                            dataColumnIndex.push(index);
+                    for(var i = 0; i < options.dataColumns.length; i++) {
+                        if(this.textContent === options.dataColumns[i]) {
+                            dataColumnIndices.push(index);
                         }
                     }
                 }
@@ -69,8 +83,8 @@
                     if(colIndex === labelColumnIndex) {
                         chartConfig.labels.push(this.textContent);
                     } else {
-                        for(var i = 0; i < dataColumnIndex.length; i++) {
-                            if(colIndex === dataColumnIndex[i]) {
+                        for(var i = 0; i < dataColumnIndices.length; i++) {
+                            if(colIndex === dataColumnIndices[i]) {
 
                                 if(chartConfig.datasets[i] === undefined) {
                                     chartConfig.datasets[i] = {};
@@ -84,10 +98,11 @@
                 });
             });
 
-            //console.log(JSON.stringify(chartConfig));
-
-            var chartReference = new Chart(document.getElementById("chart").getContext("2d")).Line(chartConfig);
-            chartReference.update();
+            console.log(JSON.stringify(chartConfig));
+            setTimeout(function(){
+                new Chart(document.getElementById(options.canvasId).getContext("2d")).Line(chartConfig, chartOptions);
+            }, 100);
+            //var chartReference = new Chart(document.getElementById(options.canvasId).getContext("2d")).Line(chartConfig);
 
         });
     }
