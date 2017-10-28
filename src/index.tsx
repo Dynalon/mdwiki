e0a346a6c92174c1fa2948062630c1ee94afcc26
Timo Dörr <timo@latecrew.de>
2017-10-28 18:51:24 +0200

    Basic markdown rendering with preact

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
 import { h, render, Component } from "preact";
+import { HtmlRenderer, Parser } from "commonmark";
 
-export class EntryComponent extends Component<{}, {}> {
+
+interface IProps {
+}
+
+interface IState {
+    html: string;
+}
+
+class EntryComponent extends Component<{}, IState> {
+    private renderArea: Element;
+
+    componentDidMount() {
+        fetch('sample.md').then((resp) => {
+            resp.text().then(text => {
+                const div = markdownToDiv(text);
+                this.renderArea.appendChild(div);
+            })
+        })
+    }
     render() {
-        return <h1>Hello world!</h1>
+        return (<div ref={(self) => this.renderArea = self}></div>);
     }
 }
 document.addEventListener("DOMContentLoaded", () => {
     render(<EntryComponent />, document.getElementById('root'))
 })
+
+function markdownToDiv(markdown: string): HTMLDivElement {
+    const div = document.createElement("div");
+    div.setAttribute("data-markdown-root", "")
+    const parsed = new Parser().parse(markdown);
+
+    const rendered = new HtmlRenderer().render(parsed);
+    div.innerHTML = rendered;
+
+    return div;
+}
